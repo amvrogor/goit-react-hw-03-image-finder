1af3ba52d77014585808aecdfd82d41d8df2194a
Alexey Gorchakov <amvrogor@gmail.com>
2023-03-05 01:41:52 +0200

    2

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const LoadMoreButton = styled.button`
+  max-width: 100px;
+  margin: 0 auto;
   padding: 8px 16px;
   border-radius: 2px;
   background-color: #3f51b5;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -13,6 +13,7 @@ export class ImageGallery extends Component {
     error: '',
     status: 'idle',
     page: 1,
+    totalHits: 0,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -22,17 +23,32 @@ export class ImageGallery extends Component {
     ) {
       this.setState({ status: 'pending' });
 
+      if (prevProps.value !== this.props.value) prevState.page = 1;
+
       pixaBayAPI(this.props.value.trim().toLowerCase(), this.state.page)
         .then(data => {
           if (data.hits.length === 0) {
+            this.setState({
+              hits: [],
+              status: 'resolved',
+              totalHits: 0,
+            });
             return toast.error(
               'Sorry, there are no images matching your search query. Please try again.'
             );
           }
-          this.setState({
-            hits: [...this.state.hits, ...data.hits],
-            status: 'resolved',
-          });
+          if (prevProps.value !== this.props.value) {
+            this.setState({
+              hits: [...data.hits],
+              status: 'resolved',
+              totalHits: data.totalHits,
+            });
+          } else {
+            this.setState({
+              hits: [...this.state.hits, ...data.hits],
+              status: 'resolved',
+            });
+          }
         })
         .catch(error => {
           console.log(error);
@@ -43,29 +59,29 @@ export class ImageGallery extends Component {
 
   handleLoad = () => {
     this.setState(prev => ({ page: prev.page + 1 }));
-    console.log(this.state.page);
   };
 
   render() {
-    const { status, hits, error } = this.state;
+    const { status, hits, error, totalHits } = this.state;
+    if (status === 'pending' && hits.length === 0) return <Loader />;
 
-    // if (status === 'pending') return <Loader />;
-
-    if (status === 'resolved')
+    if (status === 'resolved' || hits.length !== 0)
       return (
-        <ImageList>
-          {hits.map(hit => (
-            <ImageGalleryItem key={hit.id} hit={hit} />
-          ))}
-
-          <Button onLoad={this.handleLoad} />
-        </ImageList>
+        <>
+          <ImageList>
+            {hits.map(hit => (
+              <ImageGalleryItem
+                key={hit.id}
+                hit={hit}
+                onOpen={this.props.onOpen}
+              />
+            ))}
+          </ImageList>
+          {status === 'pending' ? <Loader /> : ''}
+          {totalHits > hits.length ? <Button onLoad={this.handleLoad} /> : ''}
+        </>
       );
 
     if (status === 'rejected') return <h1>{error.message}</h1>;
   }
 }
-
-/* <ul class="gallery">
-  <!-- Набор <li> с изображениями -->
-</ul> */
